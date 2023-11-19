8488470b6408a8df97b9de43df6ffa0ecb0b60be
patryq03 <138616976+patryq03@users.noreply.github.com>
2023-11-19 21:58:43 +0100

    1

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,16 +6,12 @@ const ingredients = [
   "Herbs",
   "Condiments",
 ];
-
 const list = document.querySelector("ul#ingredients");
 const elements = [];
-
 for (const items of ingredients) {
   const li = document.createElement("li");
   li.classList.add("item");
   li.textContent = items;
-  list.appendChild(li);
+  elements.push(li);
 }
 list.append(...elements);
-
-console.log(elements);
